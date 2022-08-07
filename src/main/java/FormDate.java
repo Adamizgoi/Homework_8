459cba11df83bbd8f024b10b93535d5fc2ed5386
Adamizgoi <adamizgoi97@gmail.com>
2022-08-07 19:50:36 +0300

    Initial commit

---
+++ src/main/java/FormDate.java
@@ -0,0 +1,5 @@
+public class FormDate {
+    public int day; // переменная дня
+    public int month; // переменная месяца
+    public int year; // переменная года
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name; // текстовое поле для имени
+    public String passport; // для серии и номера паспорта
+    public String patronymic; // для отчества
+    public String phone; // для телефона
+    public String surname; // для фамилии
+    public boolean subscription; // для статуса подписки
+    public FormDate birthday; // для дня рождения
+}
